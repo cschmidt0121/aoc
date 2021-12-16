bbfe0ecf4cb37bfe8f1e78b4376f816a36099fc7
Cameron Schmidt <cschmidt@hurricanelabs.com>
2021-12-16 12:23:09 -0500

    ugggggh

--- 16/b.py
+++ 16/b.py
@@ -1,9 +1,11 @@
+from functools import reduce
+
 with open("input.txt", "r") as f:
     data = f.read()
 
 bytes = bytearray.fromhex(data)
 current_bit = 0
-version_sum = 0
+bits_read = 0
 
 masks = {
     1: 0x80,
@@ -19,10 +21,22 @@ masks = {
 
 high_masks = {1: 0x01, 2: 0x03, 3: 0x07, 4: 0xF, 5: 0x1F, 6: 0x3F, 7: 0x7F, 8: 0xFF}
 
+operations = {
+    0: sum,
+    1: lambda l: reduce((lambda x, y: x * y), l),
+    2: min,
+    3: max,
+    5: lambda l: (1 if l[0] > l[1] else 0),
+    6: lambda l: (1 if l[0] < l[1] else 0),
+    7: lambda l: (1 if l[0] == l[1] else 0),
+}
+
 
 def read_bits(num_bits):
     global current_bit
     global bytes
+    global bits_read
+    bits_read += num_bits
     if current_bit + num_bits <= 8:
         byte = bytes[0]
         byte = byte << current_bit
@@ -35,7 +49,6 @@ def read_bits(num_bits):
     else:
         byte = bytes[0]
         high_bits = byte & high_masks[8 - current_bit]
-        # shift_amount = 8 - current_bit
         num_bits -= 8 - current_bit
         bytes = bytes[1:]
         current_bit = 0
@@ -66,45 +79,41 @@ def read_literal():
     return literal_value
 
 
-def handle_operator():
+def handle_operator(operation_id):
+    operation = operations[operation_id]
     length_type_id = read_bits(1)
     if length_type_id == 0:
         total_length_in_bits = read_bits(15)
-        num_sub_packets = int(total_length_in_bits / 11)
-        remainder = total_length_in_bits - (11 * num_sub_packets)
+        num_sub_packets = -1
     elif length_type_id == 1:
         num_sub_packets = read_bits(11)
-        remainder = 0
+        total_length_in_bits = -1
     current_subpacket = 0
     subpackets = []
+    global bits_read
+    bits_read_before_operator = bits_read
     while True:
-        if current_subpacket == (num_sub_packets - 1):
-            if remainder > 0:
-                parse_packet()
-
+        if current_subpacket == (num_sub_packets) or bits_read == (
+            bits_read_before_operator + total_length_in_bits
+        ):
             break
         else:
-            parse_packet()
+            subpackets.append(parse_packet())
             current_subpacket += 1
+    return operation(subpackets)
 
 
 def parse_packet():
-    global version_sum
     packet_version = read_bits(3)
-    version_sum += packet_version
     packet_type = read_bits(3)
     if packet_type == 4:
-        literal = read_literal()
+        out = read_literal()
     else:
-        handle_operator()
+        out = handle_operator(packet_type)
+    return out
 
 
-while True:
-    if len(bytes) == 0:
-        break
-    try:
-        parse_packet()
-    except IndexError:
-        break
+print(parse_packet())
 
-print(version_sum)
+
+# print(version_sum)
